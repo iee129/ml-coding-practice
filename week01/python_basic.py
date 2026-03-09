d30e1cc0f4e3b4baadc875dbd1e63e5ac6c4f7ad
iee129 <3yjmy89@gmail.com>
2026-03-09 15:21:45 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -4,3 +4,22 @@ b = 4
 # 지수승
 print(a ** b)
 print(a ** 3)
+
+# 나머지 연산
+print(a % b)
+print(7 % 3)
+
+# 나눗셈 몫 구하기
+print(a // b)
+print(7 // 3)
+
+s1 = 'Hello Python'
+print(s1)
+
+s3 = '''Hello
+Python'''
+print(s3)
+
+head = "Python"
+tail = " is fun"
+print(head + tail)
