8faae74f8faa71e80189e4d7b3cb27da7511b86d
iee129 <3yjmy89@gmail.com>
2026-03-10 01:00:40 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -303,3 +303,21 @@ while i < 5:
     i += 1
     print('*' * i)
 
+# 함수 정의
+def sum1(a, b):
+    x = a + b
+    return x
+
+def sum2(*args):
+    x = 0
+    for i in args:
+        x += i
+    return x
+
+# 함수 호출
+a = 5
+b = 3
+print(sum1(a, b))
+print(sum1(3, 5))
+print(sum2(1, 2, 3, 4, 5))
+print
