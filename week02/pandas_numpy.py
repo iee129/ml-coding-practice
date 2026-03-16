32f37df43b270db237d7a673db29299f3aea84c3
iee129 <3yjmy89@gmail.com>
2026-03-16 15:26:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -69,5 +69,15 @@ data = {
     'comment_length': [150, 200, 50, 300, 120, 180, 75, 160],
     'likes': [25, 30, 10, 45, 20, 35, 5, 28],
     'is_spam': [False, False, True, False, False, True, False, False],
-    'has_image': [True, False, True, True, False,]
+    'has_image': [True, False, True, True, False, False, True, True]
 }
+df = pd.DataFrame(data)
+print(df.head())
+
+# 필터링 조건 설정
+condition = (
+    (df['comment_length'] >= 100) &
+    (df['likes'] >= 20) &
+    (~df['is_spam']) &
+    
+)
