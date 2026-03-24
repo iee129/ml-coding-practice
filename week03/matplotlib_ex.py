e4964197b0e926e80887f5850fa89b9c7b7a6339
iee129 <3yjmy89@gmail.com>
2026-03-24 17:11:05 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -269,7 +269,10 @@ plt.plot([4, 5, 6])
 plt.subplot(2, 2, 3)
 plt.plot([7, 8, 9])
 
-pl
+plt.subplot(2, 2, 4)
+plt.plot([10, 11, 12])
+plt.savefig('Figure11.png')
+plt.close()
 
 ## **타이타닉 데이터셋으로 개별 서브플롯 하나씩 그리기**
 
