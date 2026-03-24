781ef3de649e9af3aa2eb9353047fa989970137a
iee129 <3yjmy89@gmail.com>
2026-03-24 16:46:04 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -230,4 +230,18 @@ plt.xticks([1, 2, 3], ['1st Class', '2nd Class', '3rd Class'])
 # 범례 설정
 plt.legend(violin_plot['bodies'], ['1st Class', '2nd Class', '3rd Class'],
            title='Pclass', loc="upper right")
-plt.
+plt.savefig('Figure10.png')
+plt.close()
+
+## **에러 바 : 요금의 평균과 표준편차 표현하기**
+
+# 각 부모와 자녀의 수에 대한 요금의 평균과 표준 편차 계산
+fare_means = titanic.groupby('Parch')['Fare'].mean()  # 평균
+print(fare_means, '\n')
+
+fare_std = titanic.groupby('Parch')['Fare'].std()     # 표준 편차
+print(fare_std)
+
+"""* 에러바는 데이터의 표준 편차를 나타내는 경우에는 길이가 길수록 해당 그룹의 데이터가 퍼져 있음을 의미"""
+
+# 에러바로 요금의 평균과 표준 편차 표현
