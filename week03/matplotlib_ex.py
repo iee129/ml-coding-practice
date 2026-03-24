1e66eb0c9f9b29a42bd4f046c40786de21c22926
iee129 <3yjmy89@gmail.com>
2026-03-24 16:59:04 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -265,4 +265,18 @@ plt.close()
 titanic = pd.read_csv('3.1.1.titanic.csv')
 
 # 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
-parch_counts = titanic.grop
+parch_counts = titanic.gropby('Parch')['Survived'].value_counts().unstack().fillna(0)
+print(parch_counts)
+
+# x, y 설정
+x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 반환
+y1 = parch_counts[0].values # 사망자
+y2 = parch_counts[1].values # 생존자
+
+plt.figure(figsize=(10, 10))
+
+# plt.subplot() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 하나씩 추가
+# 첫 번쨰 서브플롯 설정(선 그래프)
+plt.subplot(2, 1, 1)
+# 선 그래프 그리기
+plt.plot(x, y1, '-o', color='indigo', )
