cac6ce6be28463df043083324b9c657e540a7ecb
iee129 <3yjmy89@gmail.com>
2026-03-24 17:50:05 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -331,4 +331,11 @@ plt.close()
 ## **타이타닉 데이터셋으로 개별 서브플롯 동시에 그리기**
 
 # 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
-parch_counts = titanic.groupby('Parch')['Surci
+parch_counts = titanic.groupby('Parch')['Survived'].value_counts().unstack().fillna(0)
+print(parch_counts)
+
+# x, y값 설정
+x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 변환
+y1 = parch_counts[0].values # 사망자
+y2 = parch_counts[1].values # 생존자
+
