1225dd5d6949e52a2645904b802f16e78d01a22b
iee129 <3yjmy89@gmail.com>
2026-03-24 16:09:04 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -94,4 +94,17 @@ scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Survived'],cma
 plt.title('Age and Fare Relationship with Survival on Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
-plt.legend(*scatter.legend_elements()[0], title='Survived', )
+plt.legend(*scatter.legend_elements()[0], title='Survived',
+           labels=['Not Survived', 'Survived'], loc='upper right')
+plt.savefig('Figure04.png')
+plt.close()
+
+"""### **파이 차트 : 생존자, 사망자 비율 표현하기**"""
+
+# 사망자와 생존자의 수 계산
+survived_counts = titanic['Survived'].value_counts()
+print(survived_counts)
+
+# 파이 차트 그리기
+plt.figure(figsize=(8, 8))
+plt.pie
