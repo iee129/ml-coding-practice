8fd8a13cdbe790edd24dfd38619c24bc664ac278
iee129 <3yjmy89@gmail.com>
2026-03-25 09:53:21 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -139,7 +139,7 @@ plt.close()
 # 결측치 처리
 titanic = titanic.dropna(subset=['Age', 'Fare'])
 
-# 상관 행렬 계싼
+# 상관 행렬 계산
 correlation_matrix = titanic.drop('PassengerId', axis=1).corr(numeric_only=True)
 print(correlation_matrix)
 
@@ -148,7 +148,7 @@ plt.matshow(correlation_matrix, cmap='PuRd_r')
 plt.colorbar()
 
 # x축과 y축의 눈금 설정
-plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.colulmns, rotation=45)
+plt.xticks(range(len(correlation_matrix.columns)), correlation_matrix.columns, rotation=45)
 plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
 
 plt.title('Correlation Heatmap of Titanic')
