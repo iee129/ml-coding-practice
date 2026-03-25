ceeca1f976f066ba6182cd719429b8cd7af57ca9
iee129 <3yjmy89@gmail.com>
2026-03-25 09:50:51 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -76,7 +76,7 @@ plt.axvline(x=survived_counts['male'], color='gray', linestyle='--', linewidth=1
 for i, value in enumerate(survived_counts):
   plt.text(value + 1, i, str(value), ha='left', va='center')
 
-plt.savefig('Figure-03.png')
+plt.savefig('Figure03.png')
 plt.close()
 
 """### **산점도 그래프 : 나이와 요금, 생존 여부 확인하기**"""
