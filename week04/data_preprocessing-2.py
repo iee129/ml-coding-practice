8582bb64dc7d54b3b827e181131c1cbfea5a02ac
iee129 <3yjmy89@gmail.com>
2026-04-05 23:08:27 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -33,4 +33,6 @@ housing_labels = strat_train_set["median_house_value"].copy()
 null_rows_idx = housing.isnull().any(axis=1)
 housing.loc[null_rows_idx].head()
 
-from sklearn.impute import Simple
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleI
