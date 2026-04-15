8b0f02d7322a5b891d43f5284c7ee117bcf79faf
iee129 <3yjmy89@gmail.com>
2026-04-15 23:24:02 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -20,4 +20,24 @@ def main():
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)
+            getPostData(post, jsonResult, cnt)                # [CODE 3]
+
+        start = jsonResponse['start'] + jsonResponse['display']
+        jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
+    
+    print('전체 검색 : %d 건' %total)
+
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
+                              ensure_ascii = False)
+        
+        outfile.write(jsonFile)
+
+    print("가져온 데이터 : %d 건" %(cnt))
+    print('%s_naver_%s.json SAVED' % (srcText, node))
+
+
+def getNaverSearch(node, srcText, page_start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    
