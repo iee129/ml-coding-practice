0920fc76062f677f53a16e58fa49b3799a596fec
iee129 <3yjmy89@gmail.com>
2026-04-16 10:13:01 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -60,7 +60,7 @@ def getRequestUrl(url):
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-            print("[%s] Url Requset Success" % datetime.datetime.now())
+            print("[%s] Url Request Success" % datetime.datetime.now())
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
