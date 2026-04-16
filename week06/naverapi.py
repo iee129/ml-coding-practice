9af876928288fc7e131cd2f15e79d92a39f9dd81
iee129 <3yjmy89@gmail.com>
2026-04-16 09:45:33 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -4,4 +4,9 @@ import datetime
 import json
 
 client_id = 'n506YF7IGfKX0YbK3Bie'
-client_secret = ''
+client_secret = 'KqZGEJRtqQ'
+
+def main():
+
+    node = 'news'
+    srcText = input('검색어를 입력하세요: ')
