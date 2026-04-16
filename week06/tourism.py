e5104749914c5218931a02c07480813918b9fcc6
iee129 <3yjmy89@gmail.com>
2026-04-16 10:15:31 +0900

    Updated Changes

---
+++ week06/tourism.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+import pandas as pd
+
+ServiceKey = "자신의 Service Key"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult
