6f77484efd1f00992b4283bb3c4826fca3fd6615
iee129 <3yjmy89@gmail.com>
2026-04-16 10:21:01 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,7 +4,7 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = "자신의 Service Key"
+ServiceKey = "6dec99fec0357fe0fbdbe9a46b27f3e1eb471f47c2e2ceceb1ba719153111a89"
 
 """### [CODE 0]"""
 
@@ -15,3 +15,7 @@ def main():
     print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
     nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
     nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
+    nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
+    ed_cd = "E"
+
+    jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd)
