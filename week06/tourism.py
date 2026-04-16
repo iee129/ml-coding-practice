dbe8232214d64ba30be65217534d4937c60d6a5d
iee129 <3yjmy89@gmail.com>
2026-04-16 10:47:32 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -9,21 +9,21 @@ ServiceKey = "6dec99fec0357fe0fbdbe9a46b27f3e1eb471f47c2e2ceceb1ba719153111a89"
 """### [CODE 0]"""
 
 def main():
-    jsonResult = []
-    result = []
+  jsonResult = []
+  result = []
 
-    print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
-    nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
-    nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
-    nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
-    ed_cd = "E"
+  print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
+  nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
+  nStartYear = int(input('데이터를 몇 년부터 수집할까요? : '))
+  nEndYear = int(input('데이터를 몇 년까지 수집할까요? : '))
+  ed_cd = "E"
 
-    jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
+  jsonResult, result, natName, dataEND = getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear)  #[CODE 3]
 
-    #파일저장 : csv 파일
-    columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
-    result_df = pd.DataFrame(result, columns = columns)
-    result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
+  #파일저장 : csv 파일
+  columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
+  result_df = pd.DataFrame(result, columns = columns)
+  result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
 
 """### [CODE 3]"""
 
