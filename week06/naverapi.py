960f300c926af962493478b310bf57a323dc60df
iee129 <3yjmy89@gmail.com>
2026-04-16 09:53:03 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -41,4 +41,6 @@ def getNaverSearch(node, srcText, page_start, display):
     base = "https://openai.naver.com/v1/search"
     node = "/%s.json" % node
     parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
-    
+
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)
