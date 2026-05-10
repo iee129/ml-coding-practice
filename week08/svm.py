b6ef248ab196b8fd99b361a19ccee3d1bedccc32
iee129 <3yjmy89@gmail.com>
2026-05-10 16:28:54 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
-iris = load_iris(as_fram=True)
+iris = load_iris(as_frame=True)
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = (iris.target == 2)  # Iris virginica
 
