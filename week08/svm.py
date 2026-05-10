03927aef72b63c7e9f8d0c011b0f94a678733f8d
iee129 <3yjmy89@gmail.com>
2026-05-10 15:58:24 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -55,3 +55,13 @@ from sklearn.svm import LinearSVR
 np.random.seed(42)
 X = 2 * np.random.rand(50, 1)
 y = 4 + 3 * X[:, 0] + np.random.randn(50)
+
+svm_reg = make_pipeline(StandardScaler(),
+                        LinearSVR(epsilon=0.5, dual=True, random_state=42))
+svm_reg.fit(X, y)
+
+from sklearn.svm import SVR
+
+# 이 세 줄은 간단한 2차방정식 데이터셋을 생성합니다.
+np.random.seed(42)
+X = 2 * np.random
